947f314099826d7ef1a30a8f4f1bd10f33e9e924
vanjaandrejic <vanjaandrejic5@gmail.com>
2022-09-27 12:25:22 +0200

    update

--- 2.sort/script.js
+++ 2.sort/script.js
@@ -1,14 +1,14 @@
-arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+arr1 = [3, 8, 7, 6, 5, -4, 3, 2, 1];
 
 function sortArray(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length - i - 1; j++) {
-            if (arr[j + 1] < arr[j]) {
-                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
-            }
-        }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j + 1] < arr[j]) {
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+      }
     }
-    return arr;
+  }
+  return arr;
 }
 
 console.log(sortArray(arr1));
--- 6.iterate/script.js
+++ 6.iterate/script.js
@@ -28,24 +28,20 @@ const obj = {
   find: "hard",
 };
 
-
-"use strict";
 const words = [];
 function findWord(obj) {
-    if (Object.keys(obj).includes('find')) {
-        words.push(obj.find);
-    }
-    if (typeof obj === 'object') {
-        Object.keys(obj).forEach(key => {
-            findWord(obj[key]);
-        });
-    }
-    else if (Array.isArray(obj)) {
-        obj.forEach(o => {
-            findWord(o);
-        });
-    }
+  if (Object.keys(obj).includes("find")) {
+    words.push(obj.find);
+  }
+  if (typeof obj === "object") {
+    Object.keys(obj).forEach((key) => {
+      findWord(obj[key]);
+    });
+  } else if (Array.isArray(obj)) {
+    obj.forEach((o) => {
+      findWord(o);
+    });
+  }
 }
 findWord(obj);
 console.log(words);
-
